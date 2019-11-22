791f043549ee7dbeb48ed04c88fb2d3a3fad1b61
TJReynolds88 <tj.reynolds1988@gmail.com>
2019-11-22 16:50:52 -0600

    Add files via upload
    The begins of a Chatbot.

---
+++ Chatbot.py
@@ -0,0 +1,33 @@
+
+
+#Hello
+print("Hello. I am a Chatbot to help you find your dream car.")
+
+#Whats your name?
+name = input('Whats your name?')
+
+print("Hello ", name," , Nice to meet you")
+
+#What kind of car are you looking for
+typeOfVeh = input('What kinda of vehicle are you looking for?')
+
+print("I like " + typeOfVeh + "s their very efficent")
+
+# Year of Vehicle 
+year = input('What year?')
+
+# Color
+color = input(' Is there a certain color your looking for?')
+
+# Make 
+make = input(' Is there a certain make you like ? ')
+
+#Confirm 
+print("So ", name, ", I have a ",  year, " ", color ," ", make, " Is that correct ?")
+
+#yes or no
+
+#Thinking
+
+#API pull request
+print("Here's what I found for you on a", year, color, make,".")
